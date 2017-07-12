a8683e952fbf576deb11b459b92b3b7b29acf78f
Ahmad N. F <fahmadnaufal@gmail.com>
2017-07-12 14:55:36 +0700

    add missing import classes

--- app/Http/Controllers/Api/UserModule.php
+++ app/Http/Controllers/Api/UserModule.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Validator;
 
 use App\User;
 
