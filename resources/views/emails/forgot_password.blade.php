ea69ed6ca126c05e22d231d2bb3548d638010990
xpanda17 <alif_bhaskoro@yahoo.com>
2017-09-18 02:05:23 +0700

    fix url

--- resources/views/emails/forgot_password.blade.php
+++ resources/views/emails/forgot_password.blade.php
@@ -7,6 +7,6 @@
 	untuk mendapatkan password baru anda.
 	<br><br>
 	Jika link tidak dapat diakses, silahkan buka link di bawah ini:<br>
-	www.rp.rentuff.id/reset_password/{{$token}}
+	rp.rentuff.id/reset_password/{{$token}}
 </body>
 </html>
